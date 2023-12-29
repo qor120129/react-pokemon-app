8bef71dc1d714506af9790b367512ae177830fdb
qor120129 <qor120129@gmail.com>
2023-12-30 00:53:33 +0900

    포켓몬 검색기능 추가

---
+++ src/components/LazyImage.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+
+const LazyImage = ({ alt, url }) => {
+  const [loading, setLoading] = useState(true)
+  const [opactiy, setOpactiy] = useState('opactiy-0')
+
+  useEffect(() => {
+    loading ? setOpactiy('opactiy-0') : setOpactiy('opactiy-100')
+  }, [loading])
+
+  return (
+    <>
+      {loading && (
+        <div
+          className={`absolute h-full z-10 flex items-center justify-center`}>
+          ..loading
+        </div>
+      )}
+      <img
+        src={url}
+        alt={alt}
+        height='auto'
+        loading='lazy'
+        onLoad={() => setLoading(false)}
+        className={`h-full ${opactiy}`}
+      />
+    </>
+  )
+}
+
+export default LazyImage
